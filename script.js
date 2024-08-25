9cc7909ee1ae8f0c46e44260ab4533c14207db18
JuancaJimenezDev <jjimenezv8@miumg.edu.gt>
2024-08-24 23:00:28 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.getElementById('alertButton').addEventListener('click', function() {
+
+    alert("¡Hola! Has hecho clic en el botón.");
+    
+  
+    document.getElementById('dynamicText').textContent = "¡Has hecho clic en el botón!";
+});
